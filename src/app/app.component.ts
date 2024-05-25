e66d783ede81a3c9e3cb32f1fc84ec19b47034f5
MrKartozka <mrkartozka@mail.ru>
2024-05-25 13:01:12 +0300

    prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,8 +7,35 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angularTask';
+  leftPanelWidth: number = 860;
+  isResizing: boolean = false;
+  startX: number = 0;
+  startWidth: number = 0;
+
+  onMouseDown(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (target.id === 'resizerImg') {
+      this.isResizing = true;
+      this.startX = event.clientX;
+      this.startWidth = this.leftPanelWidth;
+      event.preventDefault();
+    }
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent): void {
+    if (this.isResizing) {
+      const dx = event.clientX - this.startX;
+      this.leftPanelWidth = this.startWidth + dx;
+    }
+  }
+
+  @HostListener('document:mouseup')
+  onMouseUp(): void {
+    this.isResizing = false;
+  }
 }
